654e6912d465c42fcb6c9317bbb2abb6426a2761
Sirozha1337 <xrenogubka@mail.ru>
2015-11-12 01:39:48 +0300

    ANIMATION'S HERE, BABY!

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -1,25 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Created by Sirozha on 03.11.2015.
  */
 public class Gameboard extends JPanel
-                       implements KeyListener{
-   // int[][] board; // Массив хранящий расположение чисел на доске
-    Tile[][] board;
-    int cell_height = 100; // размеры клеточки с числом
+                       implements KeyListener, ActionListener{
+    Tile[][] board; // Массив хранящий расположение тайлов на доске
+    int cell_height = 100; // размеры тайла
     int cell_width = 100;
-    int yoffset = 110; // Отступ от верха
+    int yoffset = 110; // Отступ от верха окна
     Tile next; // Следующее число
     boolean gameover = false; // Конец игры
     boolean exit = false;   // Выход
     boolean start = false;  // Запуск игры
+    Timer timer = new Timer(1000/60, this); // Таймер для анимации, 60 кадров в секунду
 
     Gameboard(){
     }
@@ -43,8 +43,10 @@ public class Gameboard extends JPanel
         int y = random.nextInt(4);
         board[x][y] = new Tile();
         board[x][y].setVal(random.nextInt(2)+1);
+        board[x][y].setXY(y, x);
         next = new Tile();
         next.setVal(random.nextInt(2)+1);
+        timer.start();
     }
 
 
@@ -54,6 +56,7 @@ public class Gameboard extends JPanel
         super.paintComponent(g);
         if(start) {
             paintGameBoard(g);
+            paintTiles(g);
             paintNext(g);
         }
         else{
@@ -61,12 +64,17 @@ public class Gameboard extends JPanel
                 paintInvitation(g);
             else {
                 paintGameBoard(g);
+                paintTiles(g);
                 paintNext(g);
                 paintEndGame(g);
             }
         }
     }
 
+    /***
+     * Отрисовывает предложение сыграть ещё
+     * @param g
+     */
     public void paintEndGame(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         g.setColor(Color.white);
@@ -79,6 +87,10 @@ public class Gameboard extends JPanel
         g2d.drawString("Press space to play!", 15, 240);
     }
 
+    /**
+     * Отрисовывает предложение начать игру
+     * @param g
+     */
     public void paintInvitation(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         g.setColor(Color.black);
@@ -99,12 +111,28 @@ public class Gameboard extends JPanel
                 g2d.fill(rect);
                 g2d.setColor(Color.black);
                 g2d.draw(rect);
-                if(board[i][j].val != 0)
-                    g2d.drawImage(board[i][j].sprite, j*cell_width, i*cell_height+yoffset, cell_width, cell_height, this);
             }
         }
     }
 
+    /**
+     * Рисует тайлы с цифрами
+     * @param g
+     */
+    public void paintTiles(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if(board[i][j].getVal() != 0)
+                    g2d.drawImage(board[i][j].sprite, board[i][j].x, board[i][j].y+yoffset, cell_width, cell_height, this);
+            }
+        }
+    }
+
+    /**
+     * Рисует следующий тайл над игровым полем
+     * @param g
+     */
     public void paintNext(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         g2d.setPaint(Color.white);
@@ -117,24 +145,25 @@ public class Gameboard extends JPanel
         g2d.drawImage(next.sprite, 150, 5, cell_width, cell_height, this);
     }
 
-
     /***
-     * Склеивает два числа в одно
-     * @param x
-     * @param y
-     * @param x1
-     * @param y1
-     * @param d
+     * Склеивает два тайла в один
+     * @param x - координата первого тайла
+     * @param y - координата первого тайла
+     * @param x1 - координата второго тайла
+     * @param y1- координата второго тайла
      */
-    void merge(int x, int y, int x1, int y1, char d){
+    void merge(int y, int x, int y1, int x1){
         int val;
-
-        if(board[x][y] == board[x1][y1])
-            val = board[x][y].val * 2;
-        else
-            val = board[x][y].val + board[x1][y1].val;
-        board[x][y].setVal(val); board[x1][y1].setVal(0);
+        val = board[y][x].getVal() + board[y1][x1].getVal();
+        board[y][x].setXY(x1, y1);
+        board[y][x].setVal(val);
+        board[y1][x1].setVal(0);
+        board[y][x].setNewX(x);
+        board[y][x].setNewY(y);
     }
+
+
+
 
     /***
      * Сдвиг чисел влево
@@ -143,8 +172,8 @@ public class Gameboard extends JPanel
         int k = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
-                if((board[i][j].val == board[i][j+1].val && board[i][j].val > 2) || (board[i][j].val + board[i][j+1].val == 3) || board[i][j].val == 0) {
-                    merge(i, j, i, j + 1, 'l');
+                if((board[i][j].getVal() == board[i][j+1].getVal() && board[i][j].getVal() > 2) || (board[i][j].getVal() + board[i][j+1].getVal() == 3 && board[i][j].getVal() != 3) || board[i][j].getVal() == 0) {
+                    merge(i, j, i, j + 1);
                     k++;
                 }
             }
@@ -160,14 +189,14 @@ public class Gameboard extends JPanel
         int k = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 3; j > 0; j--) {
-                if((board[i][j].val == board[i][j-1].val && board[i][j].val > 2) || (board[i][j].val + board[i][j-1].val == 3) || board[i][j].val == 0){
-                    merge(i, j, i, j-1, 'r');
+                if((board[i][j].getVal() == board[i][j-1].getVal() && board[i][j].getVal() > 2) || (board[i][j].getVal() + board[i][j-1].getVal() == 3 && board[i][j].getVal() != 3) || board[i][j].getVal() == 0){
+                    merge(i, j, i, j-1);
                     k++;
                 }
             }
         }
-        if(k > 0)
-            generate('r');
+             if(k > 0)
+                 generate('r');
     }
 
     /***
@@ -177,14 +206,14 @@ public class Gameboard extends JPanel
         int k = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 4; j++) {
-                if((board[i][j].val == board[i+1][j].val && board[i][j].val > 2) || (board[i][j].val + board[i+1][j].val == 3) || board[i][j].val == 0) {
-                    merge(i, j, i + 1, j, 'u');
+                if((board[i][j].getVal() == board[i+1][j].getVal() && board[i][j].getVal() > 2) || (board[i][j].getVal() + board[i+1][j].getVal() == 3 && board[i][j].getVal() != 3) || board[i][j].getVal() == 0) {
+                    merge(i, j, i + 1, j);
                     k++;
                 }
             }
         }
-        if(k > 0)
-            generate('u');
+            if(k > 0)
+                generate('u');
     }
 
     /***
@@ -194,14 +223,14 @@ public class Gameboard extends JPanel
         int k = 0;
         for (int i = 3; i > 0; i--) {
             for (int j = 0; j < 4; j++) {
-                if((board[i][j].val == board[i-1][j].val && board[i][j].val > 2) || (board[i][j].val + board[i-1][j].val == 3) || board[i][j].val == 0) {
-                    merge(i, j, i - 1, j, 'd');
+                if((board[i][j].getVal() == board[i-1][j].getVal() && board[i][j].getVal() > 2) || (board[i][j].getVal() + board[i-1][j].getVal() == 3 && board[i][j].getVal() != 3) || board[i][j].getVal() == 0) {
+                    merge(i, j, i - 1, j);
                     k++;
                 }
             }
         }
-        if(k > 0)
-            generate('d');
+             if(k > 0)
+                generate('d');
     }
 
     /***
@@ -215,53 +244,60 @@ public class Gameboard extends JPanel
             case 'l': ;
                 while(!generated){
                     int y = random.nextInt(4);
-                    if(board[y][3].val == 0){
-                        board[y][3].setVal(next.val);
+                    if(board[y][3].getVal() == 0){
+                        board[y][3].setVal(next.getVal());
+                        board[y][3].setXY(3, y);
                         generated = true;
                     }
                 }
             case 'r':
                 while(!generated){
                     int y = random.nextInt(4);
-                    if(board[y][0].val == 0){
-                        board[y][0].setVal(next.val);
+                    if(board[y][0].getVal() == 0){
+                        board[y][0].setVal(next.getVal());
+                        board[y][0].setXY(0, y);
                         generated = true;
                     }
                 }
             case 'u':
                 while(!generated) {
                     int x = random.nextInt(4);
-                    if (board[3][x].val == 0) {
-                        board[3][x].setVal(next.val);
+                    if (board[3][x].getVal() == 0) {
+                        board[3][x].setVal(next.getVal());
+                        board[3][x].setXY(x, 3);
                         generated = true;
                     }
                 }
             case 'd':
                 while(!generated) {
                     int x = random.nextInt(4);
-                    if (board[0][x].val == 0) {
-                        board[0][x].setVal(next.val);
+                    if (board[0][x].getVal() == 0) {
+                        board[0][x].setVal(next.getVal());
+                        board[0][x].setXY(x, 0);
                         generated = true;
                     }
                 }
         }
         int next1;
-        while((next1 = random.nextInt(3) + 1) == next.val);
+        while((next1 = random.nextInt(3) + 1) == next.getVal());
         next.setVal(next1);
         check_endgame();
     }
 
+    /***
+     * Проверка произошел ли конец игры
+     */
     void check_endgame(){
         boolean flag = true;
         for (int i = 0; i < 3 && flag; i++) {
             for (int j = 0; j < 4 && flag; j++) {
-                if((board[i][j].val == board[i+1][j].val && board[i][j].val > 2) || (board[i][j].val + board[i+1][j].val == 3) || board[i][j].val == 0)
+                if((board[i][j].getVal() == board[i+1][j].getVal() && board[i][j].getVal() > 2) || (board[i][j].getVal() + board[i+1][j].getVal() == 3) || board[i][j].getVal() == 0)
                     flag = false;
             }
         }
         for (int i = 0; i < 4 && flag; i++) {
             for (int j = 0; j < 3 && flag; j++) {
-                if((board[i][j].val == board[i][j+1].val && board[i][j].val > 2) || (board[i][j].val + board[i][j+1].val == 3) || board[i][j].val == 0)
+                if((board[i][j].getVal() == board[i][j+1].getVal() && board[i][j].getVal() > 2) || (board[i][j].getVal() + board[i][j+1].getVal() == 3) || board[i][j].getVal() == 0)
                     flag = false;
             }
         }
@@ -269,20 +305,23 @@ public class Gameboard extends JPanel
         start = !flag;
     }
 
+    /**
+     * Обработка событий клавиатуры
+     * @param e
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         if(start){
             switch(e.getKeyCode()){
-                case KeyEvent.VK_LEFT: move_left(); repaint(); break;
-                case KeyEvent.VK_RIGHT: move_right(); repaint(); break;
-                case KeyEvent.VK_UP: move_up(); repaint(); break;
-                case KeyEvent.VK_DOWN: move_down(); repaint(); break;
+                case KeyEvent.VK_LEFT: move_left(); break;
+                case KeyEvent.VK_RIGHT: move_right(); break;
+                case KeyEvent.VK_UP: move_up(); break;
+                case KeyEvent.VK_DOWN: move_down(); break;
             }
         }
         else
             if (e.getKeyCode() == KeyEvent.VK_SPACE) {
                 start();
-                repaint();
             }
         if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
             exit = true;
@@ -290,14 +329,22 @@ public class Gameboard extends JPanel
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
+    public void keyReleased(KeyEvent e) {}
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e) {}
 
+    /**
+     * Каждые x миллисекунд изменяем координаты тайлов для анимации
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                board[i][j].animate();
+            }
+        }
+        repaint();
     }
-
-
 }
--- src/Tile.java
+++ src/Tile.java
@@ -3,20 +3,57 @@ import java.awt.*;
 /**
  * Created by Sirozha on 04.11.2015.
  */
-public class Tile {
+
+public class Tile{
     int val;
+    int x, y;
+    int newx, newy;
     Image sprite;
 
     Tile(){
         val = 0;
     }
 
+    /**
+     * Устанавливает значение переменной val
+     * @param val1
+     */
     void setVal(int val1){
         val = val1;
         if(val != 0) {
             String path = "/" + val + ".png";
             sprite = Toolkit.getDefaultToolkit().getImage(getClass().getResource(path));
         }
+    }
+
+    void setXY(int x1, int y1){
+        newx = x = x1*100;
+        newy = y = y1*100;
+    }
+
+    void setNewX(int nx){
+        newx = nx * 100;
+    }
+
+    void setNewY(int ny){
+        newy = ny * 100;
+    }
+
+    int getVal(){
+        return val;
+    }
+
+    void animate(){
+        if(val == 0)
+            return;
+        if(x < newx)
+            x += 10;
+        if(x > newx)
+            x -= 10;
+        if(y < newy)
+            y += 10;
+        if(y > newy)
+            y -= 10;
     }
 
 }
